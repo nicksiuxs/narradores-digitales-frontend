5403c05c638ee37ef1a61039618da33438c0b463
Nicolas <nicksiuxs@gmail.com>
2021-06-02 21:32:59 -0500

    ADD: setup component

---
+++ src/components/SetUpRecord/SetUprecord.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./SetUprecord.css";
+
+import Header from "../Header/Header";
+
+const SetUprecord = () => {
+	return (
+		<div className="set-up-record">
+			<Header />
+			Selecciona un escenario
+		</div>
+	);
+};
+
+export default SetUprecord;
